1ec30fc9bc410008fdb81f93b0c04f359a582c7b
Thomas Borg <thomaslborg7@gmail.com>
2024-05-21 16:56:20 +0200

    rails-task-manager complete

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,12 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  # root "tasks#index"
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new", as: :new
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update"
+  get "tasks/:id", to: "tasks#show", as: :task
+  post "tasks", to: "tasks#create"
+  delete "tasks/:id", to: "tasks#destroy"
 end
